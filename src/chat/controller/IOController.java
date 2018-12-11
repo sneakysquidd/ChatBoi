112498b3ce6319a40e96c9d8a65aab8c5f81a7f9
_gavbob <gsct2002@gmail.com>
2018-12-11 13:38:20 -0700

    Added loadFile method

--- src/chat/controller/IOController.java
+++ src/chat/controller/IOController.java
@@ -8,6 +8,36 @@ import java.io.PrintWriter;
 
 public class IOController
 {
+	public static String loadFile(ChatController app, String path)
+	{
+		String contents = "";
+		
+		try
+		{
+			File saveFile = new File(path);
+			Scanner fileScanner;
+			if(saveFile.exists()) 
+			{
+				fileScanner = new Scanner(saveFile);
+				while (fileScanner.hasNext()) 
+				{
+					contents += fileScanner.nextLine() + "\n";
+				}
+			}
+		}
+		catch(IOException error)
+		{
+			app.handleErrors(error);
+		}
+		catch(Exception genericError)
+		{
+			app.handleErrors(genericError);
+		}
+		return contents;
+	}
+		
+	
+	
 	public static void saveText(ChatController app, String path, String textToSave)
 	{
 		try
